b1662fe4fca95f528d8cfd4784bdfdf7401ef597
KogaYamada <koga0087y@gmail.com>
2020-08-22 14:41:32 +0900

    initial commit

---
+++ server/server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const { json } = require('body-parser');
